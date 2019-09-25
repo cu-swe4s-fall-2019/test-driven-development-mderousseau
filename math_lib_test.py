0963020dd46817be16f33e779dabb7bf41ddc495
Mikaela DeRousseau <mikaela.derousseau@colorado.edu>
2019-09-25 13:44:21 -0600

    add test for expected value from constants

--- math_lib_test.py
+++ math_lib_test.py
@@ -6,7 +6,10 @@ class TestMathLib(unittest.TestCase):
     def test_list_mean_empty_list(self):
         r = math_lib.list_mean(None)
         self.assertEqual(r, None)
-        
+    
+    def test_mean_constants(self):
+        r = math_lib.list_mean([1,1,1,1])
+        self.assertEqual(r,1)
         
 if __name__ == '__main__':
     unittest.main()
