3f3e77c70bd76d47ef1ea2dcf548360ca3daac70
Mikaela DeRousseau <mikaela.derousseau@colorado.edu>
2019-09-25 14:26:02 -0600

    add random integers test

--- math_lib_test.py
+++ math_lib_test.py
@@ -1,5 +1,7 @@
 import unittest
 import math_lib
+import random
+import statistics
 
 class TestMathLib(unittest.TestCase):
     
@@ -10,6 +12,16 @@ class TestMathLib(unittest.TestCase):
     def test_mean_constants(self):
         r = math_lib.list_mean([1,1,1,1])
         self.assertEqual(r,1)
+        
+    def test_list_mean_rand_ints(self):
+        L = []
+        for i in range(100):
+            for j in range(10):
+                L.append(
+                random.randint(0,100))
+            r = math_lib.list_mean(L)
+            e = statistics.mean(L)
+            self.assertEqual(r,e)
         
 if __name__ == '__main__':
     unittest.main()
