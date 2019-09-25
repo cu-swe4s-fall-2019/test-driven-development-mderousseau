a3c10e1c61c1534094275bea130a5832304c195b
Mikaela DeRousseau <mikaela.derousseau@colorado.edu>
2019-09-25 14:38:44 -0600

    add random floats test

--- math_lib_test.py
+++ math_lib_test.py
@@ -2,6 +2,7 @@ import unittest
 import math_lib
 import random
 import statistics
+import math
 
 class TestMathLib(unittest.TestCase):
     
@@ -22,6 +23,16 @@ class TestMathLib(unittest.TestCase):
             r = math_lib.list_mean(L)
             e = statistics.mean(L)
             self.assertEqual(r,e)
+        
+    def test_list_mean_rand_floats(self):
+        L = []
+        for i in range(100):
+            for j in range(10):
+                L.append(
+                    random.uniform(0,100))
+            r = math_lib.list_mean(L)
+            e = statistics.mean(L)
+            self.assertTrue(math.isclose(r,e))
         
 if __name__ == '__main__':
     unittest.main()
