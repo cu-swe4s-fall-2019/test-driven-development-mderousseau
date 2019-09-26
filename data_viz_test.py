c877c430a3baa5088b676fe3429e1785f8d08a86
mderousseau <mide4448@colorad.edu>
2019-09-26 21:03:37 +0000

    add test to check file exists after

--- data_viz_test.py
+++ data_viz_test.py
@@ -1,5 +1,7 @@
 import unittest
 import data_viz
+import os.path
+from os import path
 
 class TestDataViz(unittest.TestCase):
     
@@ -14,6 +16,24 @@ class TestDataViz(unittest.TestCase):
         self.assertEqual(r, None)
         self.assertEqual(a, None)
         self.assertEqual(b, None)
+    
+    def test_file_exists_after_box(self):
+        L = [1, 2, 3, 4]
+        out_file_name = "make_file.png"
+        r = data_viz.boxplot(L, out_file_name)
+        self.assertTrue(path.exists("make_file.png"))
+        
+    def test_file_exists_after_hist(self):
+        L = [1, 2, 3, 4]
+        out_file_name = "make_file1.png"
+        r = data_viz.histogram(L, out_file_name)
+        self.assertTrue(path.exists("make_file1.png"))
+        
+    def test_file_exists_after_combo(self):
+        L = [1, 2, 3, 4]
+        out_file_name = "make_file2.png"
+        r = data_viz.combo(L, out_file_name)
+        self.assertTrue(path.exists("make_file2.png"))
    
 if __name__ == '__main__':
     unittest.main()
