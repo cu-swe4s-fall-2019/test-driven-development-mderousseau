e20d30c340a535ec5dbcf8153305a2b8fb098f5d
mderousseau <mide4448@colorad.edu>
2019-09-26 20:50:43 +0000

    test for existing file name for all methods

--- data_viz_test.py
+++ data_viz_test.py
@@ -8,8 +8,12 @@ class TestDataViz(unittest.TestCase):
         open('a_file.png', 'a').close()
         out_file_name = 'a_file.png'
         r = data_viz.boxplot(L, out_file_name)
+        a = data_viz.histogram(L, out_file_name)
+        b = data_viz.combo(L, out_file_name)
+        # repeat test in here just because its so redundant/simple
         self.assertEqual(r, None)
-        
-        
+        self.assertEqual(a, None)
+        self.assertEqual(b, None)
+   
 if __name__ == '__main__':
     unittest.main()
