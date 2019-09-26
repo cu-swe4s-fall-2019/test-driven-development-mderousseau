f0257b20710ccbfdc4a1d6c0601e200a87a6585b
Mikaela DeRousseau <mikaela.derousseau@colorado.edu>
2019-09-25 21:14:59 -0600

    fix style issues

--- math_lib_test.py
+++ math_lib_test.py
@@ -5,80 +5,82 @@ import statistics
 import math
 import numpy
 
+
 class TestMathLib(unittest.TestCase):
-    
+
     # test for empty list
     def test_list_mean_empty_list(self):
         r = math_lib.list_mean(None)
         self.assertEqual(r, None)
-    
+
     # test mean on known constants
     def test_mean_constants(self):
-        r = math_lib.list_mean([1,1,1,1])
-        self.assertEqual(r,1)
-   
+        r = math_lib.list_mean([1, 1, 1, 1])
+        self.assertEqual(r, 1)
+
     # test stdev on known constants
     def test_stdev_constants(self):
-        r = math_lib.list_stdev([1,2,3,4])
-        self.assertTrue(math.isclose(r,1.1180339887499))
-    
+        r = math_lib.list_stdev([1, 2, 3, 4])
+        self.assertTrue(math.isclose(r, 1.1180339887499))
+
     # test mean with random ints
     def test_list_mean_rand_ints(self):
         L = []
         for i in range(100):
             for j in range(10):
                 L.append(
-                random.randint(0,100))
+                random.randint(0, 100))
             r = math_lib.list_mean(L)
             e = statistics.mean(L)
-            self.assertEqual(r,e)
-        
+            self.assertEqual(r, e)
+
     # test stdev with random ints
     def test_list_stdev_rand_ints(self):
         L = []
         for i in range(100):
             for j in range(10):
                 L.append(
-                random.randint(0,100))
+                random.randint(0, 100))
             r = math_lib.list_stdev(L)
             e = numpy.std(L)
-            self.assertTrue(math.isclose(e,r, rel_tol = 1e-03))
-    
+            self.assertTrue(math.isclose(e, r, rel_tol=1e-03))
+
     def test_list_mean_rand_floats(self):
         L = []
         for i in range(100):
             for j in range(10):
                 L.append(
-                    random.uniform(0,100))
+                    random.uniform(0, 100))
             r = math_lib.list_mean(L)
             e = statistics.mean(L)
-            self.assertTrue(math.isclose(r,e))
-            
+            self.assertTrue(math.isclose(r, e))
+
     def test_list_stdev_rand_floats(self):
         L = []
         for i in range(100):
             for j in range(10):
                 L.append(
-                    random.uniform(0,100))
+                    random.uniform(0, 100))
             r = math_lib.list_mean(L)
             e = statistics.mean(L)
-            self.assertTrue(math.isclose(r,e))
-    
+            self.assertTrue(math.isclose(r, e))
+
     def test_list_mean_non_int_in_list(self):
         L = []
         for i in range(10):
-            L.append(random.randint(0,100))
+            L.append(random.randint(0, 100))
         L.append('X')
         r = math_lib.list_mean(L)
         self.assertRaises(TypeError, r)
-        
+
     def test_list_stdev_non_int_in_list(self):
         L = []
         for i in range(10):
-            L.append(random.randint(0,100))
+            L.append(random.randint(0, 100))
         L.append('X')
         r = math_lib.list_stdev(L)
         self.assertRaises(TypeError, r)
-        
+
+
 if __name__ == '__main__':
     unittest.main()
