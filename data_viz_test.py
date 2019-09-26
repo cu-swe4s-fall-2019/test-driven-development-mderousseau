1ce1180a840e4a867aa061d61dd3efa910df7d9f
mderousseau <mide4448@colorad.edu>
2019-09-26 14:24:33 +0000

    add tests for no arrays

--- data_viz_test.py
+++ data_viz_test.py
@@ -3,10 +3,21 @@ import data_viz
 
 class TestDataViz(unittest.TestCase):
     
-    def test_no_array(self):
-        out_file_name = 'out_file_name.png'
+    def test_boxplot_no_array(self):
+        out_file_name = 'out_file_box.png'
         r = data_viz.boxplot(None, out_file_name)
         self.assertEqual(r, None)
+        
+    def test_hist_no_array(self):
+        out_file_name = 'out_file_hist.png'
+        r = data_viz.histogram(None, out_file_name)
+        self.assertEqual(r, None)
+        
+    def test_both_no_array(self):
+        out_file_name = 'out_file_both.png'
+        r = data_viz.boxplot(None, out_file_name)
+        self.assertEqual(r, None)
+
         
         
 if __name__ == '__main__':
