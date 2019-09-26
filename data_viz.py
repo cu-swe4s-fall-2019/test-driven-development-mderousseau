15d9cc823073ad88fbe670e63a12a89c6b69a41e
mderousseau <mide4448@colorad.edu>
2019-09-26 20:30:36 +0000

    add test for pre-existing file

--- data_viz.py
+++ data_viz.py
@@ -1,6 +1,22 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import sys
+import os.path
+from os import path
 
 def boxplot(L, out_file_name):
-    return None
+    if path.exists(out_file_name) == True:
+        return None
+    width = 3
+    height = 3
+    fig = plt.figure(figsize=(width, height), dpi=300)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.boxplot(L)
+    plt.savefig(out_file_name, bbox_inches="tight")
+
+   
+    
 
 def histogram(L, out_file_name):
     return None
--- data_viz_test.py
+++ data_viz_test.py
@@ -3,22 +3,13 @@ import data_viz
 
 class TestDataViz(unittest.TestCase):
     
-    def test_boxplot_no_array(self):
-        out_file_name = 'out_file_box.png'
-        r = data_viz.boxplot(None, out_file_name)
+    def test_file_already_exists(self):
+        L = [1, 2, 3, 4]
+        open('a_file.png', 'a').close()
+        out_file_name = 'a_file.png'
+        r = data_viz.boxplot(L, out_file_name)
         self.assertEqual(r, None)
-        
-    def test_hist_no_array(self):
-        out_file_name = 'out_file_hist.png'
-        r = data_viz.histogram(None, out_file_name)
-        self.assertEqual(r, None)
-        
-    def test_both_no_array(self):
-        out_file_name = 'out_file_both.png'
-        r = data_viz.boxplot(None, out_file_name)
-        self.assertEqual(r, None)
-
         
         
 if __name__ == '__main__':
-    main()
+    unittest.main()
