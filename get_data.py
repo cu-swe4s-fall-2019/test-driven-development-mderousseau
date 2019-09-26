7009a13930246442aac208044614b2b8c084aaf0
Mikaela DeRousseau <mikaela.derousseau@colorado.edu>
2019-09-25 22:41:55 -0600

    test for no file

--- get_data.py
+++ get_data.py
@@ -1,3 +1,9 @@
+import sys
 
-def read_stdin_col(col_num):
-    return None
+def read_stdin_col(col_number):
+    file_name = sys.stdin
+    try:
+        file = open(file_name)
+    except FileNotFoundError:
+        raise FileNotFoundError(file_name + ' not found.')
+
